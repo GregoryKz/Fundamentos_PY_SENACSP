bfa7fa838d941262448f36a9078eafa1a564fc9c
Gregory Klaus <gregory.klaus2015@gmail.com>
2025-12-15 11:48:02 -0300

    Delete aula01 directory

--- aula01/exercicios/exercicio05.py
+++
@@ -1,5 +0,0 @@
-print (' Programa de subtração')
-numero01 = int(input('Digite o primeiro numero: '))
-numero02 = int(input('Digite o segundo numero: '))
-resultadoSubtracao = numero01-numero02
-print (f'O resultado da subtração é:{resultadoSubtracao}')
--- aula01/exercicios/exercicio06.py
+++
@@ -1,4 +0,0 @@
-print (' Programa de multiplicação')
-numero01 = int(input('Digite o primeiro numero: '))
-numero02 = int(input('Digite o segundo numero: '))
-resultadoMultiplicacao = numero01*numero02
